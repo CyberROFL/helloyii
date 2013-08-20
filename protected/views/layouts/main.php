e154250eb163e473085412da6e75c8df544269a7
CyberROFL <i.nizametdinov@gmail.com>
2013-08-21 02:02:20 +0400

    Added layout

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
+
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/common.css" media="screen, projection" />
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
+
+    <body>
+        <div id="wrap">
+            <div id="page">
+                <?php echo $content; ?>
+            </div>
+        </div>
+    </body>
+</html>
