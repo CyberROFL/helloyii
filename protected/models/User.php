1fb2b06db5be47d2f6212105a94ecc11f71f7506
CyberROFL <i.nizametdinov@gmail.com>
2013-08-21 10:52:44 +0400

    Removed deprecated repeated password check

--- protected/models/User.php
+++ protected/models/User.php
@@ -50,7 +50,6 @@ class User extends CActiveRecord
             array('email, password, company', 'length', 'max' => 100),
             array('email', 'email'),
 
-            //array('password2', 'required', 'on' => 'register'),
             array('password2', 'compare', 'compareAttribute' => 'password', 'on' => 'register'),
             array('email', 'unique'),
         );
